14e381247a5ec0a0fcc050eefa3803f5a1abbc71
paalss <paalss94@gmail.com>
2021-06-23 19:35:31 +0200

    hentet inn localstorage for å lagre token across rerenders (igjen)

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-// ny branch
+
 const AuthContext = createContext({
   token: "",
   isLoggedIn: false,
@@ -8,15 +8,18 @@ const AuthContext = createContext({
 });
 
 export const AuthContextProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {
